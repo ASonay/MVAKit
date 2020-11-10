fcd9b4ae82bcad6bb6447b401e061dee5dcf1e72
Anil Sonay <anil.sonay@cern.ch>
2020-11-10 23:05:23 +0100

    Python extension and scaling - RM Build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /afs/cern.ch/work/a/asonay/TMVA_tool
-# Build directory: /afs/cern.ch/work/a/asonay/TMVA_tool/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
