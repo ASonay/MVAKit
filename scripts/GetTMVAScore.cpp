f2dc6186ebb04d0ca7a7d72358e4e6e5df6d23cd
Anil Sonay <anil.sonay@cern.ch>
2021-07-17 13:16:03 +0200

    Adding Exc SM4t BDT

--- scripts/GetTMVAScore.cpp
+++ scripts/GetTMVAScore.cpp
@@ -48,37 +48,44 @@ void GetTMVAScore(int mH=400,int mva_index=0)
     cout << "Your library, " << lib << " cannot be loaded." << endl;
     exit(0);
   }
-  map<int,string> mva_loc;
-  string conf_loc;
+  map<int,pair<string,string>> file_loc;
   if (mva_index==0) {
-    mva_loc[1] = "/afs/cern.ch/work/a/asonay/public/SM4top_BDT/loader_0/factory_BDT_0_Score.weights.xml,/afs/cern.ch/work/a/asonay/public/SM4top_BDT/loader_1/factory_BDT_1_Score.weights.xml";
-    conf_loc = "/afs/cern.ch/work/a/asonay/public/SM4top_BDT/config_SM4top_bdt.conf";
+    file_loc[1].first = "/afs/cern.ch/work/a/asonay/public/SM4top_BDT/loader_0/factory_BDT_0_Score.weights.xml,/afs/cern.ch/work/a/asonay/public/SM4top_BDT/loader_1/factory_BDT_1_Score.weights.xml";
+    file_loc[1].second = "/afs/cern.ch/work/a/asonay/public/SM4top_BDT/config_SM4top_bdt.conf";
   }
   else if (mva_index==1) {
-    mva_loc[1] = "/eos/user/k/ktam/public/Htt/BDT/PFlow_Setup/training/1L/VarSet1/ge9jge3b/M"+to_string(mH)+"/loader_0/weights/factory_BDT_0_Score.weights.xml,/eos/user/k/ktam/public/Htt/BDT/PFlow_Setup/training/1L/VarSet1/ge9jge3b/M"+to_string(mH)+"/loader_1/weights/factory_BDT_1_Score.weights.xml";
-    conf_loc = "/eos/user/k/ktam/public/Htt/BDT/PFlow_Setup/training/1L/VarSet1/config_bdt_crossvalidation.conf";
+    file_loc[91].first = "/afs/cern.ch/work/a/asonay/public/SM4top_BDT/exclusive/9jge3b/loader_0/weights/factory_BDT_0_Score.weights.xml,/afs/cern.ch/work/a/asonay/public/SM4top_BDT/exclusive/9jge3b/loader_1/weights/factory_BDT_1_Score.weights.xml";
+    file_loc[101].first = "/afs/cern.ch/work/a/asonay/public/SM4top_BDT/exclusive/ge10jge3b/loader_0/weights/factory_BDT_0_Score.weights.xml,/afs/cern.ch/work/a/asonay/public/SM4top_BDT/exclusive/ge10jge3b/loader_1/weights/factory_BDT_1_Score.weights.xml";
+    file_loc[91].second = "/afs/cern.ch/work/a/asonay/public/SM4top_BDT/exclusive/9jge3b/config_SM4top_bdt.conf";
+    file_loc[101].second = "/afs/cern.ch/work/a/asonay/public/SM4top_BDT/exclusive/ge10jge3b/config_SM4top_bdt.conf";
   }
   else if (mva_index==2) {
-    mva_loc[1] = "/eos/user/k/ktam/public/Htt/BDT/PFlow_Setup/training/1L/VarSet6_QGTagging_eta/ge9jge3b/M"+to_string(mH)+"/loader_0/weights/factory_BDT_0_Score.weights.xml,/eos/user/k/ktam/public/Htt/BDT/PFlow_Setup/training/1L/VarSet6_QGTagging_eta/ge9jge3b/M"+to_string(mH)+"/loader_1/weights/factory_BDT_1_Score.weights.xml";
-    conf_loc = "/eos/user/k/ktam/public/Htt/BDT/PFlow_Setup/training/1L/VarSet6_QGTagging_eta/config_bdt_crossvalidation.conf";
+    file_loc[1].first = "/eos/user/k/ktam/public/Htt/BDT/PFlow_Setup/training/1L/VarSet1/ge9jge3b/M"+to_string(mH)+"/loader_0/weights/factory_BDT_0_Score.weights.xml,/eos/user/k/ktam/public/Htt/BDT/PFlow_Setup/training/1L/VarSet1/ge9jge3b/M"+to_string(mH)+"/loader_1/weights/factory_BDT_1_Score.weights.xml";
+    file_loc[1].second = "/eos/user/k/ktam/public/Htt/BDT/PFlow_Setup/training/1L/VarSet1/config_bdt_crossvalidation.conf";
   }
   else if (mva_index==3) {
-    mva_loc[91] = "/eos/user/b/bsm4tops/bsm4teos/MVA_study_1LOS/BDT_weights_212120_v2/ljets_9jge3b/M"+to_string(mH)+"/loader_0/weights/factory_BDT_0_Score.weights.xml,/eos/user/b/bsm4tops/bsm4teos/MVA_study_1LOS/BDT_weights_212120_v2/ljets_9jge3b/M"+to_string(mH)+"/loader_1/weights/factory_BDT_1_Score.weights.xml";
-    mva_loc[101] = "/eos/user/b/bsm4tops/bsm4teos/MVA_study_1LOS/BDT_weights_212120_v2/ljets_ge10jge3b/M"+to_string(mH)+"/loader_0/weights/factory_BDT_0_Score.weights.xml,/eos/user/b/bsm4tops/bsm4teos/MVA_study_1LOS/BDT_weights_212120_v2/ljets_ge10jge3b/M"+to_string(mH)+"/loader_1/weights/factory_BDT_1_Score.weights.xml";
-    conf_loc = "/afs/cern.ch/work/a/asonay/public/config_bdt_crossvalidation_pflow.conf";
+    file_loc[1].first = "/eos/user/k/ktam/public/Htt/BDT/PFlow_Setup/training/1L/VarSet6_QGTagging_eta/ge9jge3b/M"+to_string(mH)+"/loader_0/weights/factory_BDT_0_Score.weights.xml,/eos/user/k/ktam/public/Htt/BDT/PFlow_Setup/training/1L/VarSet6_QGTagging_eta/ge9jge3b/M"+to_string(mH)+"/loader_1/weights/factory_BDT_1_Score.weights.xml";
+    file_loc[1].second = "/eos/user/k/ktam/public/Htt/BDT/PFlow_Setup/training/1L/VarSet6_QGTagging_eta/config_bdt_crossvalidation.conf";
+  }
+  else if (mva_index==4) {
+    file_loc[91].first = "/eos/user/b/bsm4tops/bsm4teos/MVA_study_1LOS/BDT_weights_212120_v2/ljets_9jge3b/M"+to_string(mH)+"/loader_0/weights/factory_BDT_0_Score.weights.xml,/eos/user/b/bsm4tops/bsm4teos/MVA_study_1LOS/BDT_weights_212120_v2/ljets_9jge3b/M"+to_string(mH)+"/loader_1/weights/factory_BDT_1_Score.weights.xml";
+    file_loc[101].first = "/eos/user/b/bsm4tops/bsm4teos/MVA_study_1LOS/BDT_weights_212120_v2/ljets_ge10jge3b/M"+to_string(mH)+"/loader_0/weights/factory_BDT_0_Score.weights.xml,/eos/user/b/bsm4tops/bsm4teos/MVA_study_1LOS/BDT_weights_212120_v2/ljets_ge10jge3b/M"+to_string(mH)+"/loader_1/weights/factory_BDT_1_Score.weights.xml";
+    file_loc[91].second = "/afs/cern.ch/work/a/asonay/public/config_bdt_crossvalidation_pflow.conf";
+    file_loc[101].second = "/afs/cern.ch/work/a/asonay/public/config_bdt_crossvalidation_pflow.conf";
   }
   else {
     cout << "NO Valid option defined!"
 	 << "\n0 = SM4top"
-	 << "\n1 = Inclusive Standard BDT"
-	 << "\n2 = Inclusive Standard BDT+Q/G"
-	 << "\n3 = Exclusive Standard BDT+Q/G" << endl;
-    mva_loc[1] = "/afs/cern.ch/work/a/asonay/public/SM4top_BDT/loader_0/factory_BDT_0_Score.weights.xml,/afs/cern.ch/work/a/asonay/public/SM4top_BDT/loader_1/factory_BDT_1_Score.weights.xml";
-    conf_loc = "/afs/cern.ch/work/a/asonay/public/SM4top_BDT/config_SM4top_bdt.conf";
+	 << "\n1 = SM4top Exclusive BDT"
+	 << "\n2 = Inclusive Standard BDT"
+	 << "\n3 = Inclusive Standard BDT+Q/G"
+	 << "\n4 = Exclusive Standard BDT+Q/G" << endl;
+    file_loc[1].first = "/afs/cern.ch/work/a/asonay/public/SM4top_BDT/loader_0/factory_BDT_0_Score.weights.xml,/afs/cern.ch/work/a/asonay/public/SM4top_BDT/loader_1/factory_BDT_1_Score.weights.xml";
+    file_loc[1].second = "/afs/cern.ch/work/a/asonay/public/SM4top_BDT/config_SM4top_bdt.conf";
   }
 
-  for (auto const &x : mva_loc)
-    {evaluater[x.first] = shared_ptr<EvalTMVA>(new EvalTMVA(x.second,conf_loc));}
+  for (auto const &x : file_loc)
+    {evaluater[x.first] = shared_ptr<EvalTMVA>(new EvalTMVA(x.second.first,x.second.second));}
   
   for (auto const &x : evaluater){
     x.second->Init();
