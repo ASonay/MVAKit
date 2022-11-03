312ceed75720a67a6f2bbf9f9ee9d2f919b8208e
Anil Sonay <anil.sonay@cern.ch>
2022-11-03 12:42:13 +0100

    h5 Parser Update

--- share/h5parser.py
+++ share/h5parser.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 # load and evaluate a saved model
-from pymva.kit import MVAKit
-from pymva.extra import *
+import pymva
+#from pymva.extra import *
 import ROOT as root
 import argparse
 from sklearn.preprocessing import StandardScaler
@@ -14,41 +14,45 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser("NN keras output to txt format", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-p","--path",action="store", dest="path", help="path where the keras model file (h5) saved", required=True)
-    parser.add_argument("-c","--conf",action="store", dest="conf", help="Configuration", required=True)
-  
+    parser.add_argument("-c","--conf",action="store", dest="conf", help="Configuration", required=False)
+    parser.add_argument("-s","--doScaler",action="store_true", dest="doScaler", help="Decode your scaler too?",default=False, required=False)
     args = parser.parse_args()
 
     path = args.path
     config = args.conf
 
-    tool = MVAKit('Training')
-    tool.isClassification(0)
-    tool.isExe(0)
-    tool.SetConf(config)
-    tool.ReadConf()
-
     import tensorflow as tf
     model_file=path+'keras_output/model/model_0.h5'
+    model_output = path+'keras_output/model/model_0.txt'
+    if '.h5' in path:
+        model_file = path
+        model_output = path.replace('.h5','.txt')
     print ('MODEL : ',model_file)
     model=tf.keras.models.load_model(model_file,
                                      custom_objects=None,
                                      compile=False)
-    """
-    scaler = load(path+'keras_output/feature_weight/fold0_std_scaler.bin')
+    if args.doScaler:
+        tool = pymva.kit.MVAKit('Training')
+        tool.isClassification(0)
+        tool.isExe(0)
+        tool.SetConf(config)
+        tool.ReadConf()
     
-    x = [[0,7,132797.15,66186.695,45201.695,37540.648,30212.027,29805.652,25537.996,0,0,0,73784.015,51631.25,1.2]]
-    x_s = scaler.transform(x)
+        scaler = load(path+'keras_output/feature_weight/fold0_std_scaler.bin')
+    
+        x = [[0,7,132797.15,66186.695,45201.695,37540.648,30212.027,29805.652,25537.996,0,0,0,73784.015,51631.25,1.2]]
+        x_s = scaler.transform(x)
 
-    print (x)
-    print (x_s)
+        print (x)
+        print (x_s)
 
-    print('Normalization:\n')
-    with open(path+'keras_output/feature_weight/fold0_std_scaler.txt', 'w') as fout:
-        for i,var in enumerate(tool.Variables):
-            fout.write(var + ' ' + str(scaler.mean_[i]) + ' ' + str(scaler.scale_[i]) + '\n')
-            print ('%-40s | %-6.2e | %-6.2e'%(var,scaler.mean_[i],scaler.scale_[i]))
-    """
-    with open(path+'keras_output/model/model_0.txt', 'w') as fout:
+        print('Normalization:\n')
+        with open(path+'keras_output/feature_weight/fold0_std_scaler.txt', 'w') as fout:
+            for i,var in enumerate(tool.Variables):
+                fout.write(var + ' ' + str(scaler.mean_[i]) + ' ' + str(scaler.scale_[i]) + '\n')
+                print ('%-40s | %-6.2e | %-6.2e'%(var,scaler.mean_[i],scaler.scale_[i]))
+
+    with open(model_output, 'w') as fout:
         for ind,layer in enumerate(model.layers):
             fout.write('Start for layer ' + str(ind) + '\n')
             W = layer.get_weights()[0]
