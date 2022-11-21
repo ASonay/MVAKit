87d1b30b8f7eb3375285d59710e409f57eae5bfb
Anil Sonay <anil.sonay@cern.ch>
2022-11-21 02:34:16 +0100

    Updating HF Norm

--- scripts/preReweighting.cpp
+++ scripts/preReweighting.cpp
@@ -36,24 +36,38 @@ double rew_HF_PhHw(int nlep, int hf_class, int truth_corr) {
 }
 
 double rew_HF_nominal(int nlep, int hf_class, int truth_corr, int is4fs=0) {
-  if (hf_class==-1)
-    {return 1.60;}
-  else if (hf_class==0 && truth_corr<=2)
-    {
-      if (nlep==1)
-	{return 0.83;}
-      else if (nlep==2)
-	{return 0.87;}
-      else
-	{return 0.0;}
-    }
-  else if ((hf_class == 1 || (hf_class == 0 && truth_corr>2)))
-    {
-      if (is4fs)
-	{return 1.17;}
-      else
-	{return 1.21;}
-    }
-  else
-    {return 0.0;}
+  if (is4fs) {
+    if (hf_class==-1)
+      {return 1.70;}
+    else if (hf_class==0 && truth_corr<=2)
+      {
+	if (nlep==1)
+	  {return 0.83;}
+	else if (nlep==2)
+	  {return 0.86;}
+	else
+	  {return 0.0;}
+      }
+    else if ((hf_class == 1 || (hf_class == 0 && truth_corr>2)))
+      {return 1.14;}
+    else
+      {return 0.0;}
+  }
+  else {
+    if (hf_class==-1)
+      {return 1.61;}
+    else if (hf_class==0 && truth_corr<=2)
+      {
+	if (nlep==1)
+	  {return 0.84;}
+	else if (nlep==2)
+	  {return 0.87;}
+	else
+	  {return 0.0;}
+      }
+    else if ((hf_class == 1 || (hf_class == 0 && truth_corr>2)))
+      {return 1.21;}
+    else
+      {return 0.0;}
+  }
 }
